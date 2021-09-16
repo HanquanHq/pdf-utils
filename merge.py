d6cbde3b306c901cf7c9bc1bf5ab53abaa157f5e
gongluyang <15801016967@163.com>
2021-09-16 22:54:50 +0800

    add image transparency and scale

--- merge.py
+++ merge.py
@@ -5,46 +5,49 @@ from PyPDF2 import PdfFileWriter, PdfFileReader
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import A4
 
-img_path = 'img-raw/1.png'
-raw_pdf_path = "pdf-raw/test.pdf"
-target_pdf_path = "target/output.pdf"
-
-def resize():
-    src = cv2.imread(img_path, cv2.IMREAD_UNCHANGED)
-    scale_percent = 50
-    width = int(src.shape[1] * scale_percent / 100)
-    height = int(src.shape[0] * scale_percent / 100)
-    dsize = (width, height)
-    output = cv2.resize(src, dsize)
-    cv2.imwrite(img_path, output)
+IMG_PATH = 'img-raw/example.png'
+RAW_PDF_PATH = "pdf-raw/test.pdf"
+TARGET_PDF_PATH = "target/output.pdf"
+HEIGHT_SCALE = 5
+WIDTH_SCALE = 10
 
 def gen_pdf():
-    path = img_path
+    path = IMG_PATH
     img_pdf = PdfFileWriter()
     
-    # convert img to pdf
-    imgTemp = BytesIO()
-    imgDoc = canvas.Canvas(imgTemp, pagesize=A4)
-    imgDoc.drawImage(path.format(0), 0, 0) # left bottom corner
-    imgDoc.save()
-    img_pdf.addPage(PdfFileReader(BytesIO(imgTemp.getvalue())).getPage(0))
-
     # read raw pdf and write to new pdf
-    raw_pdf = PdfFileReader(open(raw_pdf_path, "rb"))
+    raw_pdf = PdfFileReader(open(RAW_PDF_PATH, "rb"))
     page_nums = raw_pdf.getNumPages()
     output = PdfFileWriter()
     for num in range(0, page_nums):
         output.addPage(raw_pdf.getPage(num))
+    
+    # calcute width 
+    last_page = output.getPage(page_nums - 1)
+    if last_page.get('/Rotate', 0) in [90, 270]:
+        height, width = last_page['/MediaBox'][2], last_page['/MediaBox'][3]
+    else:
+        height, width = last_page['/MediaBox'][3], last_page['/MediaBox'][2]
+    print(height)
+    print(width)
+
+    # convert img to pdf
+    imgTemp = BytesIO()
+    imgDoc = canvas.Canvas(imgTemp, pagesize=A4)
+    # imgDoc.setFillAlpha(0.1) # image transparency
+    imgDoc.drawImage(path, 10, 10, int(HEIGHT_SCALE/5), int(WIDTH_SCALE/10), mask='auto') # left bottom corner
+    imgDoc.save()
+    img_pdf.addPage(PdfFileReader(BytesIO(imgTemp.getvalue())).getPage(0))
 
     # merge
-    output.getPage(page_nums - 1).mergePage(img_pdf.getPage(0))
+    last_page.mergePage(img_pdf.getPage(0))
 
     # write
-    outputStream = open(target_pdf_path, "wb")
+    outputStream = open(TARGET_PDF_PATH, "wb")
     output.write(outputStream)
     outputStream.close()
 
 
 if __name__ == '__main__':
-    resize()
+    # resize()
     gen_pdf()
