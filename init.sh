b57c6ada44b3783b8629ea2ede3a36533332348a
gongluyang <15801016967@163.com>
2021-09-16 23:07:52 +0800

    init.sh

---
+++ init.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+if [ ! -d "./img-raw" ]; then
+      mkdir img-raw
+fi
+
+if [ ! -d "./pdf-raw" ]; then
+      mkdir pdf-raw
+fi
+
+if [ ! -d "./target" ]; then
+      mkdir target
+fi
