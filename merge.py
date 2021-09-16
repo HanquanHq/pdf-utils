848065421d82577f0860366f963f8c2ef42d4861
gongluyang <15801016967@163.com>
2021-09-16 18:41:12 +0800

    test png

--- merge.py
+++ merge.py
@@ -1,35 +1,50 @@
 # -*- coding: utf-8 -*-
+import cv2
 from io import BytesIO
 from PyPDF2 import PdfFileWriter, PdfFileReader
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import A4
 
+img_path = 'img-raw/1.png'
+raw_pdf_path = "pdf-raw/test.pdf"
+target_pdf_path = "target/output.pdf"
+
+def resize():
+    src = cv2.imread(img_path, cv2.IMREAD_UNCHANGED)
+    scale_percent = 50
+    width = int(src.shape[1] * scale_percent / 100)
+    height = int(src.shape[0] * scale_percent / 100)
+    dsize = (width, height)
+    output = cv2.resize(src, dsize)
+    cv2.imwrite(img_path, output)
+
 def gen_pdf():
-    path = 'img-raw/chrome.png'
-    new_pdf = PdfFileWriter()
-
-    for num in range(1, 3):  # for each slide
-        # Using ReportLab Canvas to insert image into PDF
-        imgTemp = BytesIO()
-        imgDoc = canvas.Canvas(imgTemp, pagesize=A4)
-        # Draw image on Canvas and save PDF in buffer
-        imgDoc.drawImage(path.format(num), -0, -0) # from left bottom corner
-        # x, y - start position
-        imgDoc.save()
-        # Use PyPDF to merge the image-PDF into the template
-        new_pdf.addPage(PdfFileReader(BytesIO(imgTemp.getvalue())).getPage(0))
+    path = img_path
+    img_pdf = PdfFileWriter()
     
-    existing_pdf = PdfFileReader(open("pdf-raw/test.pdf", "rb"))
-    page = existing_pdf.getPage(0)
-    page.mergePage(new_pdf.getPage(0))
+    # convert img to pdf
+    imgTemp = BytesIO()
+    imgDoc = canvas.Canvas(imgTemp, pagesize=A4)
+    imgDoc.drawImage(path.format(0), 0, 0) # left bottom corner
+    imgDoc.save()
+    img_pdf.addPage(PdfFileReader(BytesIO(imgTemp.getvalue())).getPage(0))
 
+    # read raw pdf and write to new pdf
+    raw_pdf = PdfFileReader(open(raw_pdf_path, "rb"))
+    page_nums = raw_pdf.getNumPages()
     output = PdfFileWriter()
-    output.addPage(page)
-    # finally, write "output" to a real file
-    outputStream = open("output.pdf", "wb")
+    for num in range(0, page_nums):
+        output.addPage(raw_pdf.getPage(num))
+
+    # merge
+    output.getPage(page_nums - 1).mergePage(img_pdf.getPage(0))
+
+    # write
+    outputStream = open(target_pdf_path, "wb")
     output.write(outputStream)
     outputStream.close()
 
 
 if __name__ == '__main__':
+    resize()
     gen_pdf()
